1a6508297e45d3298a2958a17935e5112f34d75c
jaubourg <j@ubourg.net>
2014-05-26 12:08:18 +0200

    Makes sure @/ URLs work in routes

--- test/unit/resolvedPath.js
+++ test/unit/resolvedPath.js
@@ -4,7 +4,8 @@ module.exports = {
 	"wires.json": {
 		"path": "./lib",
 		"resolvedPath": "@/lib",
-		"resolvedCMDPath": ">/lib"
+		"resolvedCMDPath": ">/lib",
+		":resolvedRoute": "@/lib/test"
 	},
 	"resolvedPath.unit.js": function() {
 
@@ -12,26 +13,33 @@ module.exports = {
 
 		module.exports = {
 			test: function( __ ) {
-				__.expect( 3 );
+				__.expect( 4 );
 				__.strictEqual( require( "#path" ), "./lib" );
 				__.strictEqual( require( "#resolvedPath" ), path.resolve( __dirname, "./lib" ) );
 				__.strictEqual( require( "#resolvedCMDPath" ), path.resolve( process.cwd(), "./lib" ) );
+				__.strictEqual( require( ":resolvedRoute" ), "lib/test" );
 				__.done();
 			}
 		};
 
 	},
 	"/lib": {
+		"test.js": function() {
+
+			module.exports = "lib/test";
+
+		},
 		"resolvedPathSub.unit.js": function() {
 
 			var path = require( "path" );
 
 			module.exports = {
 				test: function( __ ) {
-					__.expect( 3 );
+					__.expect( 4 );
 					__.strictEqual( require( "#path" ), "./lib" );
 					__.strictEqual( require( "#resolvedPath" ), path.resolve( __dirname ) );
 					__.strictEqual( require( "#resolvedCMDPath" ), path.resolve( process.cwd(), "./lib" ) );
+					__.strictEqual( require( ":resolvedRoute" ), "lib/test" );
 					__.done();
 				}
 			};
