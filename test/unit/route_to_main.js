28689bea858bed2b749f5667adec2da72d63d1cc
jaubourg <j@ubourg.net>
2014-05-27 17:38:48 +0200

    Ensures compatibility of routing with require's pathing system

---
+++ test/unit/route_to_main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+	"wires.json": {
+		":fs": "fs"
+	},
+	"route_to_main.unit.js": function() {
+
+		module.exports = {
+			test: function( __ ) {
+				__.expect( 1 );
+				__.strictEqual( require( ":fs" ), require( "fs" ) );
+				__.done();
+			}
+		};
+
+	}
+};
