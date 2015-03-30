6410a16fc28ddde2be1e8af41c3ce4d46ed2fe0f
jaubourg <j@ubourg.net>
2015-03-30 17:21:03 +0200

    Adds command line override tests (already detected a bug)

---
+++ test/commandLineOverride.unit.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var commandLineOverride = require( "../lib/config/commandLineOverride" );
+
+module.exports = {
+	test: function( __ ) {
+		__.expect( 2 );
+		var tmp = commandLineOverride( [
+			"--arg",
+			"(flag)",
+			"hello",
+			"(!unflag)",
+			"(bool=true,string='true',otherString='hello')",
+			"tmp",
+			"tmp2",
+			"--help",
+			"(object.flag2,!object.unflag2,object.string=world,object.int=3)",
+			"end"
+		] );
+		__.deepEqual( tmp.argv, [
+			"--arg",
+			"hello",
+			"tmp",
+			"tmp2",
+			"--help",
+			"end"
+		], "argv properly cleaned up" );
+		__.deepEqual( tmp.data, {
+			flag: true,
+			unflag: false,
+			bool: true,
+			string: "true",
+			otherString: "hello",
+			object: {
+				flag2: true,
+				unflag2: false,
+				string: "world",
+				int: 3
+			}
+		}, "data properly constructed" );
+		__.done();
+	}
+};
