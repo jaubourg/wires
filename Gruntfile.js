d233d7cba7de2ab0e6fbf83ae2eab77f43d1d169
jaubourg <j@ubourg.net>
2015-03-30 16:18:12 +0200

    Integrates coverage in main GruntFile and ensures better cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,20 @@
 "use strict";
 
+var fs = require( "fs" );
+var path = require( "path" );
+var wrench = require( "wrench" );
+
+function command( expr ) {
+	expr = expr.split( " " );
+	expr[ 0 ] = path.resolve( __dirname, "node_modules/.bin/" + expr[ 0 ] + ( path.sep === "/" ? "" : ".cmd" ) );
+	return expr.join( " " );
+}
+
 module.exports = function( grunt ) {
 
+	var lcov = "data.lcov";
+	var lib = path.resolve( __dirname, "lib" );
+	var libSave = lib + "-save";
 	var lintTargets = [ "*.js", "lib/**/*.js", "test/**/*.js" ];
 
 	grunt.initConfig( {
@@ -18,8 +31,17 @@ module.exports = function( grunt ) {
 			}
 		},
 		shell: {
+			coveralls: {
+				command: command( "coveralls < " + lcov )
+			},
+			jscoverage: {
+				command: command( "jscoverage " + lib + " " + libSave )
+			},
 			test: {
 				command: "node bin/wires test"
+			},
+			testWithCoverage: {
+				command: "node bin/wires test --reporter=lcov > " + lcov
 			}
 		}
 	} );
@@ -31,4 +53,21 @@ module.exports = function( grunt ) {
 	grunt.registerTask( "lint", [ "jscs", "jshint" ] );
 	grunt.registerTask( "default", [ "lint", "shell:test" ] );
 
-};
+	// Coverage
+	grunt.registerTask( "coverage-file-manipulation", function() {
+		fs.renameSync( lib, "__coverage_tmp" );
+		fs.renameSync( libSave, lib );
+		fs.renameSync( "__coverage_tmp", libSave );
+		process.on( "exit", function() {
+			wrench.rmdirSyncRecursive( lib );
+			fs.renameSync( libSave, lib );
+			fs.unlinkSync( lcov );
+		} );
+	} );
+
+	grunt.registerTask( "coverage", [
+		"shell:jscoverage",
+		"coverage-file-manipulation",
+		"shell:testWithCoverage",
+		"shell:coveralls"
+	] );};
