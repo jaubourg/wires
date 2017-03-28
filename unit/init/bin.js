5a376f78f8333d1b97444f61399ac748b92aa882
jaubourg <j@ubourg.net>
2017-03-28 02:16:18 +0200

    Simplifies caller filename retrieval

--- unit/init/bin.js
+++ unit/init/bin.js
@@ -2,5 +2,5 @@
 
 // wires has been installed globally by the binary
 // so we call the require of the module that called us
-const whereAmI = require( `../../lib/util/whereAmI` );
-global.wires = string => require.cache[ whereAmI( -1 ).getFileName() ].require( string );
+const whoCalled = require( `../../lib/util/whoCalled` );
+global.wires = string => require.cache[ whoCalled() ].require( string );
