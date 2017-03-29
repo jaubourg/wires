fe74ccf0dbda5717ada379f8806e3ac66a125e3f
jaubourg <j@ubourg.net>
2017-03-29 15:46:42 +0200

    Has whoCalled return the module object directly

--- unit/init/bin.js
+++ unit/init/bin.js
@@ -3,4 +3,4 @@
 // wires has been installed globally by the binary
 // so we call the require of the module that called us
 const whoCalled = require( `../../lib/util/whoCalled` );
-global.wires = string => require.cache[ whoCalled() ].require( string );
+global.wires = string => whoCalled().require( string );
