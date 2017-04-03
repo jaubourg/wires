6f138e63aa289023bd35970eaf7362bcb9d7f0a2
jaubourg <j@ubourg.net>
2017-04-03 22:04:03 +0200

    Always use istanbul programmatically

--- test/cover.js
+++ test/cover.js
@@ -1,23 +1,34 @@
+/* eslint-disable no-console */
+
 "use strict";
 
-const exec = require( `./exec` );
 const fs = require( `fs` );
 const fse = require( `fs-extra` );
+const istanbul = require( `istanbul` );
+const path = require( `path` );
 
-const footer = ( file, foot ) => fs.writeFileSync( file, fs.readFileSync( file, `utf8` ) + foot, `utf8` );
+const recursiveChange = ( dir, changer ) => {
+    fs.readdirSync( dir ).forEach( basename => {
+        const filename = path.join( dir, basename );
+        if ( fs.statSync( filename ).isDirectory() ) {
+            recursiveChange( filename, changer );
+        } else if ( path.extname( filename ) === `.js` ) {
+            fs.writeFileSync( filename, changer( fs.readFileSync( filename, `utf8` ), filename ), `utf8` );
+        }
+    } );
+};
 
-const base = `${ __dirname }/..`;
+const base = path.resolve( `${ __dirname }/..` );
 
-const lib = `${ base }/lib`;
-const save = `${ base }/_save`;
-const tmp = `${ base }/_tmp`;
+const lib = path.join( base, `lib` );
+const save = path.join( base, `_save` );
 
-const instrumentSave = [
-    `${ lib }/bin.js`,
-    `${ lib }/index.js`,
-];
+const collectors = new Set( [
+    path.join( lib, `bin.js` ),
+    path.join( lib, `index.js` ),
+] );
 
-const coverageCollection = `
+const collection = `
 process.on( "exit", () => require( "fs" ).writeFileSync(
     ${ JSON.stringify( `${ base }/coverage.` ) } + Date.now() + ".json",
     JSON.stringify( __coverage__ ),
@@ -29,28 +40,32 @@ const rCoverage = /^coverage\.[0-9]+\.json$/;
 
 module.exports = {
     "instrument": () => {
+        const now = Date.now();
         fse.copySync( lib, save );
-        return exec( `@istanbul instrument --output ${ tmp } ${ lib }` )().then( () => {
-            fse.copySync( tmp, lib );
-            fse.removeSync( tmp );
-            for ( const file of instrumentSave ) {
-                footer( file, coverageCollection );
-            }
-        } );
+        const instr = new istanbul.Instrumenter();
+        recursiveChange(
+            lib,
+            ( content, file ) => instr.instrumentSync( content, file ) + ( collectors.has( file ) ? collection : `` )
+        );
+        console.log( `instrumented in ${ Date.now() - now }ms` );
     },
     "report": () => {
+        const now = Date.now();
         fse.copySync( save, lib );
         fse.removeSync( save );
-        const istanbul = require( `istanbul` );
         const collector = new istanbul.Collector();
         const reporter = new istanbul.Reporter();
         for ( const file of fs.readdirSync( base ) ) {
             if ( rCoverage.test( file ) ) {
-                collector.add( require( `${ base }/${ file }` ) );
-                fse.removeSync( `${ base }/${ file }` );
+                const filename = path.join( base, file );
+                collector.add( require( filename ) );
+                fse.removeSync( filename );
             }
         }
         reporter.addAll( [ `lcov`, `clover` ] );
-        return new Promise( resolve => reporter.write( collector, false, resolve ) );
+        return new Promise( resolve => reporter.write( collector, false, () => {
+            console.log( `coverage report generated in ${ Date.now() - now }ms` );
+            resolve();
+        } ) );
     },
 };
