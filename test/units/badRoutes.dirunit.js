edd1bbd20c2de67fde9b2656c95dc60dd07b9e8d
jaubourg <j@ubourg.net>
2017-04-03 23:49:33 +0200

    Tests bad routes

---
+++ test/units/badRoutes.dirunit.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+    "/nonString": {
+        "wires.json": {
+            ":boolean": true,
+        },
+        "nonStringRoute.unit.js"() {
+            module.exports = {
+                "non string route"( __ ) {
+                    __.expect( 1 );
+                    __.throws( () => require( `:boolean` ) );
+                    __.done();
+                },
+            };
+        },
+    },
+    "/empty": {
+        "wires.json": {
+            ":empty": ``,
+        },
+        "emptyRoute.unit.js"() {
+            module.exports = {
+                "empty route"( __ ) {
+                    __.expect( 1 );
+                    __.throws( () => require( `:empty` ) );
+                    __.done();
+                },
+            };
+        },
+    },
+};
