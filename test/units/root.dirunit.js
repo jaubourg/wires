afadd73fd9a5a2397ebae0867dc208d25b3c150b
jaubourg <j@ubourg.net>
2017-04-05 16:38:25 +0200

    Tests root directive

---
+++ test/units/root.dirunit.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = {
+    "wires.json": {
+        "person": {
+            "name": `John`,
+        },
+    },
+    "/sub": {
+        "wires.json": {
+            "@root": true,
+            "age": 27,
+        },
+        "root.unit.js"() {
+            module.exports = {
+                test( __ ) {
+                    __.expect( 1 );
+                    __.deepEqual( require( `#` ), {
+                        "age": 27,
+                    } );
+                    __.done();
+                },
+            };
+        },
+    },
+    "/subWrong": {
+        "wires.json": {
+            "@root": `true`,
+            "age": 27,
+        },
+        "root.unit.js"() {
+            module.exports = {
+                "wrong type"( __ ) {
+                    __.expect( 1 );
+                    __.throws( () => require( `#` ) );
+                    __.done();
+                },
+            };
+        },
+    },
+    "/withNamespace": {
+        "wires.json": {
+            "@namespace": ` person `,
+            "@root": true,
+            "age": 27,
+        },
+        "root.unit.js"() {
+            module.exports = {
+                "with namespace"( __ ) {
+                    __.expect( 1 );
+                    __.deepEqual( require( `#` ), {
+                        "age": 27,
+                    } );
+                    __.done();
+                },
+            };
+        },
+    },
+};
