06dc91dc2f905ad77b9ee52c31d2f065a76e819c
jaubourg <j@ubourg.net>
2017-04-06 03:21:27 +0200

    Rethinks require override so that require.resolve can be used. Actually simplifies things a lot.

---
+++ test/units/resolve.dirunit.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+    "wires.json": {
+        "lib": `lib`,
+
+        ":dir/": `./lib/`,
+        ":indirect": `./{#lib}`,
+        ":route": `./lib`,
+    },
+    "/lib": {
+        "index.js"() {
+            // empty
+        },
+    },
+    "test.unit.js"() {
+        const index = require( `path` ).resolve( __dirname, `lib`, `index.js` );
+
+        module.exports = {
+            test( __ ) {
+                __.expect( 4 );
+                __.strictEqual( require.resolve( `:dir/index` ), index );
+                __.strictEqual( require.resolve( `:indirect` ), index );
+                __.strictEqual( require.resolve( `:route` ), index );
+                __.strictEqual( require.resolve( `::./lib` ), index );
+                __.done();
+            },
+        };
+    },
+};
