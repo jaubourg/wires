8c486497b46a79510a9a8ebf764b240e063dd91d
jaubourg <j@ubourg.net>
2017-04-11 04:12:42 +0200

    Makes sure source tree is in a correct state when something fails while determining coverage

--- test/runner/cover.js
+++ test/runner/cover.js
@@ -44,10 +44,34 @@ process.on( "exit", () => {
 const rCoverage = /^coverage\.[0-9]+\.json$/;
 const rStart = /^("use strict";)?/;
 
+let libRestored = false;
+const restoreLib = () => {
+    if ( !libRestored ) {
+        console.log( `restoring lib` );
+        libRestored = true;
+        fse.copySync( save, lib );
+        fse.removeSync( save );
+    }
+};
+
+let coverageRemoved = false;
+const removeCoverage = () => {
+    if ( !coverageRemoved ) {
+        console.log( `removing temp coverage files` );
+        for ( const file of fs.readdirSync( base ) ) {
+            if ( rCoverage.test( file ) ) {
+                fse.removeSync( path.join( base, file ) );
+            }
+        }
+    }
+};
+
 module.exports = {
     "instrument": () => {
         const now = Date.now();
         fse.copySync( lib, save );
+        process.on( `exit`, removeCoverage );
+        process.on( `exit`, restoreLib );
         const instr = new istanbul.Instrumenter();
         recursiveChange(
             lib,
@@ -60,8 +84,7 @@ module.exports = {
     },
     "report": () => {
         const now = Date.now();
-        fse.copySync( save, lib );
-        fse.removeSync( save );
+        restoreLib();
         const collector = new istanbul.Collector();
         const reporter = new istanbul.Reporter();
         for ( const file of fs.readdirSync( base ) ) {
@@ -71,6 +94,7 @@ module.exports = {
                 fse.removeSync( filename );
             }
         }
+        coverageRemoved = true;
         reporter.addAll( [ `lcov`, `clover` ] );
         return new Promise( resolve => reporter.write( collector, false, () => {
             console.log( `coverage report generated in ${ Date.now() - now }ms` );
