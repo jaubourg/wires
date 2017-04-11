a1c605cdec14ab295f8d714d28557df87d5b26cb
jaubourg <j@ubourg.net>
2017-04-11 03:46:59 +0200

    Strengthens base tests by testing paths, objects & arrays cloning and null/undefined differencing

--- test/units/common/base.dirunit.js
+++ test/units/common/base.dirunit.js
@@ -2,9 +2,17 @@
 
 module.exports = {
     "wires.json": {
-        "key": `value`,
+        "array": [ 1, 2, 3 ],
         "false": false,
+        "key": `value`,
         ":module": `./test.js`,
+        "null": null,
+        "object": {
+            "field": `value`,
+            "sub": {
+                "subField": `subValue`,
+            },
+        },
     },
     "test.js"() {
         module.exports = {};
@@ -15,14 +23,31 @@ module.exports = {
     "base.unit.js"() {
         module.exports = {
             data( __ ) {
-                __.expect( 2 );
-                __.strictEqual( require( `#key` ), `value` );
+                __.expect( 7 );
+                __.deepEqual( require( `#array` ), [ 1, 2, 3 ] );
+                __.strictEqual( require( `#array.length` ), 3 );
                 __.strictEqual( require( `#false` ), false );
+                __.strictEqual( require( `#key` ), `value` );
+                __.strictEqual( require( `#null` ), null );
+                __.strictEqual( require( `#object.field` ), `value` );
+                __.strictEqual( require( `#object.sub.subField` ), `subValue` );
                 __.done();
             },
             "undefined data"( __ ) {
-                __.expect( 1 );
+                __.expect( 3 );
                 __.strictEqual( require( `#notDefined` ), undefined );
+                __.strictEqual( require( `#null.notDefined` ), undefined );
+                __.strictEqual( require( `#object.notDefined` ), undefined );
+                __.done();
+            },
+            "data is cloned"( __ ) {
+                __.expect( 4 );
+                for ( const expression of [ `#array`, `#object` ] ) {
+                    const first = require( expression );
+                    const second = require( expression );
+                    __.notStrictEqual( first, second );
+                    __.deepEqual( first, second );
+                }
                 __.done();
             },
             "environment data"( __ ) {
