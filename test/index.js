5b48611f47991f822d3a73036fd283bc39badb92
jaubourg <j@ubourg.net>
2019-04-27 00:46:21 +0200

    test: run unit tests with tracer on.

--- test/index.js
+++ test/index.js
@@ -12,9 +12,28 @@ runner( [
     // test command line
     execUnits( `cli` ),
     // test with binary
-    execUnits( `common`, true ),
+    execUnits( `common`, {
+        "bin": true,
+    } ),
+    // test with binary and trace
+    execUnits( `common`, {
+        "bin": true,
+        "trace": true,
+    } ),
     // test with binary, no env
-    execUnits( `common`, true, `` ),
+    execUnits( `common`, {
+        "bin": true,
+        "wiresEnv": ``,
+    } ),
+    // test with binary and trace, no env
+    execUnits( `common`, {
+        "bin": true,
+        "wiresEnv": ``,
+    } ),
     // test with local
     execUnits( `common` ),
+    // test with local and trace
+    execUnits( `common`, {
+        "trace": true,
+    } ),
 ] );
