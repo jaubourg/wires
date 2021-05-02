cff7c54fdadb0884596ede2d70b84a7baed0339e
jaubourg <j@ubourg.net>
2021-05-02 02:22:26 +0200

    Renames and move things around.

---  build.js
+++  build.js
@@ -4,8 +4,8 @@ const { generate } = require( `peggy` );
 const { readFileSync, writeFileSync } = require( `fs` );
 const { resolve } = require( `path` );
 
-const targetDir = resolve( __dirname, `../lib/config/parse` );
-const grammar = readFileSync( resolve( targetDir, `grammar.pegjs` ), `utf8` );
+const targetDir = resolve( __dirname, `lib/config/parse` );
+const grammar = readFileSync( resolve( targetDir, `grammar.peggy` ), `utf8` );
 writeFileSync( resolve( targetDir, `index.js` ), generate( grammar, {
     "format": `commonjs`,
     "output": `source`,
