4f3361d6b8f278cccab8bc7c89387eb1a8b8459c
jaubourg <j@ubourg.net>
2022-04-07 18:10:41 +0200

    Minor refactoring for clarity's and simplicity's sake.

--- loader.mjs
+++ loader.mjs
@@ -5,7 +5,30 @@ import { generateModule, DEFAULT_EXPORT } from "./lib/generateModule.js";
 import { deBypass, getConfig, isBypass, isRoute, isValue } from "./lib/base.js";
 import nodeVersion from "./lib/nodeVersion.js";
 
-const getItem = ( dir, expression, isPath ) => getConfig( dir ).get( expression, isPath, false );
+const marker = `file:///wires${ DEFAULT_EXPORT }:`;
+
+// node 16+
+export const load = ( url, context, defaultLoad ) => {
+    const tmp = url.startsWith( marker ) && JSON.parse( url.slice( marker.length ) );
+    if ( tmp ) {
+        const [ directory, expression ] = tmp;
+        return {
+            "format": `module`,
+            "source": generateModule( getConfig( directory ).get( expression ) ),
+        };
+    }
+    return defaultLoad( url, context );
+};
+
+// node 12 and 14
+export const getFormat = ( nodeVersion.major < 16 ) && ( ( url, context, defaultGetFormat ) => (
+    url.startsWith( marker ) ? {
+        "format": `module`,
+    } : defaultGetFormat( url, context )
+) );
+export const getSource = ( nodeVersion.major < 16 ) && load;
+
+// all versions
 
 const rDir = /^file:\/\/(.+)\/[^/]*$/;
 const getDirectory = ( { parentURL } ) => {
@@ -16,42 +39,16 @@ const getDirectory = ( { parentURL } ) => {
     return process.cwd();
 };
 
-const getPath = ( expr, context, defaultResolve ) =>
-    defaultResolve( getItem( getDirectory( context ), expr, true ).getValue() );
-
-const wiresMarker = `file:///wires${ DEFAULT_EXPORT }:`;
-
-// node 16+
-export const load = ( url, context, defaultLoad ) => {
-    const tmp = url.startsWith( wiresMarker ) && JSON.parse( url.slice( wiresMarker.length ) );
-    if ( tmp ) {
-        const [ directory, expression ] = tmp;
-        return {
-            "format": `module`,
-            "source": generateModule( getItem( directory, expression ) ),
-        };
-    }
-    return defaultLoad( url, context );
-};
-
-// node 12 and 14
-export const getFormat = ( nodeVersion.major < 16 ) && ( ( url, context, defaultGetFormat ) => (
-    url.startsWith( wiresMarker ) ? {
-        "format": `module`,
-    } : defaultGetFormat( url, context )
-) );
-export const getSource = ( nodeVersion.major < 16 ) && load;
-
 export const resolve = ( specifier, context, defaultResolve ) => {
     if ( isBypass( specifier ) ) {
         return defaultResolve( deBypass( specifier ), context );
     }
     if ( isRoute( specifier ) ) {
-        return getPath( specifier, context, defaultResolve );
+        return defaultResolve( getConfig( getDirectory( context ) ).get( specifier, true ).getValue() );
     }
     if ( isValue( specifier ) ) {
         return {
-            "url": `${ wiresMarker }${ JSON.stringify( [ getDirectory( context ), specifier ] ) }`,
+            "url": `${ marker }${ JSON.stringify( [ getDirectory( context ), specifier ] ) }`,
         };
     }
     return defaultResolve( specifier, context );
