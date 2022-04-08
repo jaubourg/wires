4c58a831be2d0b64b36752e2e009eb4145fbb352
jaubourg <j@ubourg.net>
2022-04-08 17:07:06 +0200

    Properly lints files when testing.

--- test/runner/lint.js
+++ test/runner/lint.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const { ESLint } = require( `eslint` );
+const formatter = require( `eslint-formatter-codeframe` );
 const linter = new ESLint();
 
-module.exports = () => {
+module.exports = async () => {
     console.log( `linting` );
-    const report = linter.lintFiles( `${ __dirname }/../..` );
-    if ( report.errorCount || report.warningCount ) {
-        console.log( linter.getFormatter( `codeframe` )( report.results ) );
-        if ( report.errorCount ) {
-            throw new Error( `linting failed` );
-        }
+    const reports = await linter.lintFiles( `${ __dirname }/../..` );
+    let hasError = false;
+    for ( let i = 0; !hasError && ( i < reports.length ); ++i ) {
+        hasError = ( reports[ i ].errorCount || reports[ i ].warningCount );
+    }
+    if ( hasError ) {
+        console.log( formatter( reports ) );
+        throw new Error( `linting failed` );
     }
 };
